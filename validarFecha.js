e0e85652e22c419b4d161e0bb5efe66621637036
zurimokato <noj2304@gmail.com>
2018-03-20 19:48:14 -0500

    Funcionalidad

--- validarFecha.js
+++ validarFecha.js
@@ -2,40 +2,8 @@ function Fecha(dia, mes, anio) {
 	this.dia=dia;
 	this.mes=mes;
 	this.anio=anio;
-	this.vectMes=[31,28,31,30,31,30,31,31,30,31,30,31]
+	var vectMes=[31,28,31,30,31,30,31,31,30,31,30,31];
 
-	this.Fecha(){
-		this.dia=01;
-		this.mes=01;
-		this.anio=1900;
-	}
-	this.validarFecha(){
-		if (this.anio<1900 && this.dia<0 && this.mes <0) {
-			alert(La Fecha es incorrecta);
-		}else{
-			for (var i = 0; i < vectMes.length; i++) {
-				if(this.mes==i){
-					if (this.dia==vectMes[i]) {
-						alert(la fecha esta correcta);
-					}else{
-						alert(La Fecha es incorrecta);
-					}
-				}
-				if(this.mes==i && i == 2){
-					if (isBisiesto()) {
-						if (this.mes==vectMes[i]+1) {
-							alert(la fecha esta correcta);
-						}else{
-							alert(La Fecha es incorrecta);
-						}
-					}
-				}
-			}
-			
-		}
-
-		
-	}
 
 	this.obtenerDia=function(){
 		alert(this.dia);
@@ -47,10 +15,43 @@ function Fecha(dia, mes, anio) {
 	this.obtenerAnio=function(){
 		alert(this.anio);
 	}
-	this.isBisiesto(){
+
+	this.Fecha=function(){
+		this.dia=01;
+		this.mes=01;
+		this.anio=1900;
+	}
+	this.validarFecha=function(){
+		if (this.anio<1900 && this.dia<0 && this.mes <0) {
+			alert("La Fecha es incorrecta");
+		}else{
+			for (var i = 0; i < vectMes.length; i++) {
+				if(this.mes==i){
+				if (this.dia<=vectMes[i]) {
+						alert("la fecha esta correcta");
+					}else{
+						alert("La Fecha es incorrecta");
+					}
+				}	
+				if(this.mes==i && i == 2){
+					if (this.isBisiesto()==true) {
+						if (this.mes<=vectMes[i]+1) {
+							alert("Es biesiesto y la fecha esta correcta");
+						}else{
+							alert("Es bisiesto y la Fecha es incorrecta");
+						}
+					}
+				}
+			}	
+		}
+	}
+
+	this.isBisiesto=function(){
 		var bisiesto=false;
 		if(this.anio%100!=0 && this.anio%4==0 || this.anio%400==0){
 			bisiesto=true;
+		}else{
+			bisiesto=false;
 		}
 		return bisiesto;
 	}
@@ -58,7 +59,7 @@ function Fecha(dia, mes, anio) {
 
 
 
-function validarFecha(){
+function vFecha(){
 	var dia=parseInt(document.getElementById("Dia").value);
 	var mes=parseInt(document.getElementById("Mes").value);
 	var anio = parseInt(document.getElementById("Anio").value);
