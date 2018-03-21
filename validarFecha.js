e667e535925abcf44d5cc0cdbd53eba7a6eecabb
zurimokato <noj2304@gmail.com>
2018-03-20 19:27:03 -0500

    Definiendo los metodos

--- validarFecha.js
+++ validarFecha.js
@@ -2,11 +2,39 @@ function Fecha(dia, mes, anio) {
 	this.dia=dia;
 	this.mes=mes;
 	this.anio=anio;
+	this.vectMes=[31,28,31,30,31,30,31,31,30,31,30,31]
 
-
+	this.Fecha(){
+		this.dia=01;
+		this.mes=01;
+		this.anio=1900;
+	}
 	this.validarFecha(){
-		alert(La Fecha es incorrecta);
-		alert(la fecha esta correcta);
+		if (this.anio<1900 && this.dia<0 && this.mes <0) {
+			alert(La Fecha es incorrecta);
+		}else{
+			for (var i = 0; i < vectMes.length; i++) {
+				if(this.mes==i){
+					if (this.dia==vectMes[i]) {
+						alert(la fecha esta correcta);
+					}else{
+						alert(La Fecha es incorrecta);
+					}
+				}
+				if(this.mes==i && i == 2){
+					if (isBisiesto()) {
+						if (this.mes==vectMes[i]+1) {
+							alert(la fecha esta correcta);
+						}else{
+							alert(La Fecha es incorrecta);
+						}
+					}
+				}
+			}
+			
+		}
+
+		
 	}
 
 	this.obtenerDia=function(){
@@ -20,15 +48,22 @@ function Fecha(dia, mes, anio) {
 		alert(this.anio);
 	}
 	this.isBisiesto(){
-		var bisiesto=true;
+		var bisiesto=false;
+		if(this.anio%100!=0 && this.anio%4==0 || this.anio%400==0){
+			bisiesto=true;
+		}
 		return bisiesto;
 	}
 }
 
 
 
-function operar(){
-	var numA=parseInt(document.getElementById("numA").value);
-	var numB=parseInt(document.getElementById("numB").value);
-	var operando = document.getElementById("mySelect").value;
-	}
+function validarFecha(){
+	var dia=parseInt(document.getElementById("Dia").value);
+	var mes=parseInt(document.getElementById("Mes").value);
+	var anio = parseInt(document.getElementById("Anio").value);
+
+	var fecha=new Fecha(dia,mes,anio);
+
+	fecha.validarFecha();
+}
